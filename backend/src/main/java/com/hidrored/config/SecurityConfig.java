e5eb7c3804b8f5ecae454f264f3e9990dc79eebd
SergioCalcinaMuchica <scalcinamu@unsa.edu.pe>
2025-07-22 00:26:27 -0500

    Error handling

--- backend/src/main/java/com/hidrored/config/SecurityConfig.java
+++ backend/src/main/java/com/hidrored/config/SecurityConfig.java
@@ -7,6 +7,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+
+
+
 @Configuration
 public class SecurityConfig {
 
@@ -17,11 +20,16 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    try{
     http
         .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(authz -> authz
             .requestMatchers("/api/**").permitAll()
             .anyRequest().authenticated());
     return http.build();
+    } catch (Exception e) {
+      // Se captura el error y se lanza uno con un mensaje personalizado
+      throw new RuntimeException("Error configuring security", e);
+    }
   }
 }
