0f188faf8492bbc2a93a94a401c40662c51285e0
SergioCalcinaMuchica <scalcinamu@unsa.edu.pe>
2025-07-22 00:30:26 -0500

    Creating a new file for the exception

---
+++ backend/src/main/java/com/hidrored/exception/SecurityConfigurationException.java
@@ -0,0 +1,7 @@
+package com.hidrored.exception;
+
+public class SecurityConfigurationException extends RuntimeException {
+    public SecurityConfigurationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
